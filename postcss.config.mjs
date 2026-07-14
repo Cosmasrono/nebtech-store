706140c45ba9954d9caba9b41b273551f23b4268
Cosmas <ccosmas001@gmail.com>
2026-07-14 16:23:05 +0300

    changes

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,11 +1,1 @@
-<<<<<<< HEAD
-const config = {
-  plugins: {
-    "@tailwindcss/postcss": {},
-  },
-};
-
-export default config;
-=======
 export default { plugins: { tailwindcss: {}, autoprefixer: {} } };
->>>>>>> e44a6e2 (Initial snapshot before TypeScript migration)
